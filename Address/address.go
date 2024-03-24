8ffe5121feb204b4567733be934500aedca6e4a4
Ajay Tiwary <ajay4java@gmail.com>
2024-03-24 15:50:52 +0530

    Address And Commenrs

---
+++ Address/address.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("A4-104,KunalIcon,Pune")
+}
---
+++ Comments/comment.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//comments
+	fmt.Println("Ajay")
+}
