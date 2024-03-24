59a2d186d9c4ccdf8a7cfabbb2994b7f09832e63
Ajay Tiwary <ajay4java@gmail.com>
2024-03-24 20:45:33 +0530

    loop

---
+++ Array/array.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = []string{"A", "B", "C"}
+	fmt.Println(a)
+}
---
+++ Loop/loop1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	b := []int{1, 2, 3, 4, 5}
+
+	for _, v := range b {
+		fmt.Println(v)
+
+	}
+}
